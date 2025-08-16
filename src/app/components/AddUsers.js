7cf44939a39a1b77dd1c52ff713c0be19a076141
CoolShivas <shivas2710cool00@gmail.com>
2025-08-17 00:46:12 +0530

    Getting data from input field to State.

--- src/app/components/AddUsers.js
+++ src/app/components/AddUsers.js
@@ -1,12 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const AddUsers = () => {
+  const [inputValue, setInputValue] = useState("");
+  const handlerOnAddBtn = () => {
+    console.log(inputValue); // Getting data on Browser Console;
+  };
   return (
     <>
       <center>
         <h1>Add Users</h1>
-        <input type="text" placeholder="add new users" required />
-        <button>Add</button>
+        <input
+          type="text"
+          placeholder="add new users"
+          required
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handlerOnAddBtn}>Add</button>
       </center>
     </>
   );
