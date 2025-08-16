a80785ba40c8a406561e70ab995ac9ab372a7468
CoolShivas <shivas2710cool00@gmail.com>
2025-08-16 20:47:33 +0530

    Formation of components and imported to pageJS file to display on screen.

---
+++ src/app/components/AddUsers.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddUsers = () => {
+  return (
+    <>
+      <center>
+        <h1>Add Users</h1>
+        <input type="text" placeholder="add new users" required />
+        <button>Add</button>
+      </center>
+    </>
+  );
+};
+
+export default AddUsers;
---
+++ src/app/components/DisplayUsers.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DisplayUsers = () => {
+  return (
+    <>
+      <center>
+        <h1>Display Users</h1>
+        <ul>
+          <li>Shiva</li>
+        </ul>
+      </center>
+    </>
+  );
+};
+
+export default DisplayUsers;
