66d31a051c7bcfd731e40720f1c2f912957c21a1
CoolShivas <shivas2710cool00@gmail.com>
2025-08-19 20:49:44 +0530

    Instead of only using the simple reducer in store now using the usersData as reducer. And, resolving all issues/errors related to this.

--- src/app/components/DisplayUsers.js
+++ src/app/components/DisplayUsers.js
@@ -5,8 +5,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeUsers } from "../redux/slice";
 
 const DisplayUsers = () => {
-  const getData = useSelector((data) => data.users);
-  // console.log(getData);
+  // const getData = useSelector((data) => data);
+  // console.log(getData); // Getting on Browser Console as :- {usersData: {…}}usersData: users: [{…}][[Prototype]]: Object[[Prototype]]: Object
+
+  // const getData = useSelector((data) => data.usersData);
+  // console.log(getData); // Getting on Browser Console as :- {users: Array(1)}users: [{…}][[Prototype]]: Object
+
+  const getData = useSelector((data) => data.usersData.users);
+  console.log(getData);
+  // // //  Getting on Browser Console as :-
+  // // //  [{…}]
+  // // //  0: {id: 'rYzXe5BOfBWD6DFRAzR0l', name: 'umar'}
+  // // //  length: 1
+  // // //  [[Prototype]]: Array(0)
+
   const dispatch = useDispatch();
 
   return (
--- src/app/redux/store.js
+++ src/app/redux/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducer from "./slice";
+import usersReducer from "./slice";
 
 export const store = configureStore({
-  reducer: reducer,
+  reducer: {
+    usersData: usersReducer,
+  },
 });
